0368d3a970c2693f23a19245d698c19141a52292
asandoval2313 <asandoval2313@gmail.com>
2024-06-19 14:43:49 -0700

    first commit

---
+++ ui/src/components/UpdateTaskForm.js
@@ -0,0 +1,55 @@
+import { Dialog, DialogTitle, TextField } from "@mui/material";
+import React, { useState } from "react";
+import CheckIcon from "@mui/icons-material/Check";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { API_URL } from "../utils";
+
+export const UpdateTaskForm = ({
+  isDialogOpen,
+  setIsDialogOpen,
+  task,
+  fetchTasks,
+}) => {
+  const { id, completed } = task;
+  const [taskName, setTaskName] = useState("");
+
+  const handleUpdateTaskName = async () => {
+    try {
+      await axios.put(API_URL, {
+        id,
+        name: taskName,
+        completed,
+      });
+
+      await fetchTasks();
+
+      setTaskName("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Dialog open={isDialogOpen}>
+      <DialogTitle>Edit Task</DialogTitle>
+      <div className="dialog">
+        <TextField
+          size="small"
+          label="Task"
+          variant="outlined"
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={async () => {
+            await handleUpdateTaskName();
+            setIsDialogOpen(false);
+          }}
+        >
+          <CheckIcon />
+        </Button>
+      </div>
+    </Dialog>
+  );
+};
